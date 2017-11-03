97c54c49c8ffb8c98cdca652d132bb93688ce1ac
tvrc4m <tvrc4m@qq.com>
2017-11-03 17:34:28 +0800

    add workflow image

--- controller/doc.cls.php
+++ controller/doc.cls.php
@@ -8,8 +8,25 @@ class DocController extends Doc {
      */
     public function develop(){
 
-        $api_list=['开发文档'=>['develop'=>['name'=>'开发规范','url'=>'/doc/develop#develop']]];
+        $api_list=$this->getDocList();
 
         $this->display('doc/develop.html',['api_list'=>$api_list,'tab_selected'=>'doc']);
+    }
+
+    public function workflow(){
+
+        $api_list=$this->getDocList();
+
+        $this->display('doc/workflow.html',['api_list'=>$api_list,'tab_selected'=>'workflow']);
+    }
+
+    private function getDocList(){
+
+        return [
+            '开发文档'=>[
+                'develop'=>['name'=>'开发规范','url'=>'/doc/develop#develop'],
+                'workflow'=>['name'=>'工作流','url'=>'/doc/workflow#workflow']
+            ]
+        ];
     }
 }
