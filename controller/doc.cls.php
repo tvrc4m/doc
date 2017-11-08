c5a9c043e3fab7a69e9e22b3434237d5fe3e4e07
tvrc4m <tvrc4m@qq.com>
2017-11-08 15:54:18 +0800

    完善doc文档请求

--- controller/doc.cls.php
+++ controller/doc.cls.php
@@ -2,22 +2,13 @@
 
 class DocController extends Doc {
 
-    /**
-     * 开发规范
-     * @return 
-     */
-    public function develop(){
+    public function index($params){
+
+        $doc=$params['doc'];
 
         $api_list=$this->getDocList();
 
-        $this->display('doc/develop.html',['api_list'=>$api_list,'tab_selected'=>'doc']);
-    }
-
-    public function workflow(){
-
-        $api_list=$this->getDocList();
-
-        $this->display('doc/workflow.html',['api_list'=>$api_list,'tab_selected'=>'workflow']);
+        $this->display("doc/{$doc}.html",['api_list'=>$api_list,'tab_selected'=>$doc]);
     }
 
     private function getDocList(){
@@ -26,6 +17,9 @@ class DocController extends Doc {
             '开发文档'=>[
                 'develop'=>['name'=>'开发规范','url'=>'/doc/develop#develop'],
                 'workflow'=>['name'=>'工作流','url'=>'/doc/workflow#workflow']
+            ],
+            'APP与JS交互文档'=>[
+                'app2js'=>['name'=>'app与js交互','url'=>'/doc/app2js#app2js'],
             ]
         ];
     }
