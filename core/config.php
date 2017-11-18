00f43b8918b244539cb251677824495404bc0fe6
tvrc4m <tvrc4m@qq.com>
2017-11-19 02:00:54 +0800

    修改数据库链接参数

--- core/config.php
+++ core/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define("MYSQL_HOST","127.0.0.1");
-define("MYSQL_NAME", 'root');
-define("MYSQL_PWD", 'mac@mysql');
+define("MYSQL_NAME", 'doc');
+define("MYSQL_PWD", 'doc');
 define("MYSQL_DB", 'doc');
